8872fdc3c3666f7b507230436ce5a17f846c7918
Mathieu Brun-Picard <b00673732@essec.edu>
2018-01-16 22:43:53 +0100

    With comments

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -3,6 +3,7 @@ class Timer
   attr_accessor :seconds
   #write your code here
 
+#Je defini les secondes = 0 de base
   def initialize
     @seconds = 0
     return @seconds
@@ -14,14 +15,14 @@ class Timer
 
 #Une heure = 3600s
 #Une minute = 60s
-#On calcule les temps
+#On calcule les temps en prenant en compte cette base la
 
       @heures = @seconds/3600.to_i
       @minutes = (@seconds/60 - @heures*60).to_i
       @seconds =  (@seconds - @minutes*60 - @heures*3600).to_i
 
       #Boucle d'affichage en fonction de nombre de secondes
-
+#Y a surement moyen de simplifier la boucle mais je ne vois pas comment
 if @minutes >10
       @minutes = "#{@minutes}"
     else
@@ -42,7 +43,7 @@ if @minutes >10
 
 
     end
-
+#Resultat finale
     return "" + @heures +":" + @minutes +":" + @seconds
   end
 end
