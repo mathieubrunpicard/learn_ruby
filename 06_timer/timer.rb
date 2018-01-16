b58afa4bfb585fd123cb9a3ae3e627ebf6a5c9a1
Mathieu Brun-Picard <b00673732@essec.edu>
2018-01-16 17:49:12 +0100

    Exercice 6

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,48 @@
 class Timer
+
+  attr_accessor :seconds
   #write your code here
+
+  def initialize
+    @seconds = 0
+    return @seconds
+  end
+
+  def time_string
+
+
+
+#Une heure = 3600s
+#Une minute = 60s
+#On calcule les temps
+
+      @heures = @seconds/3600.to_i
+      @minutes = (@seconds/60 - @heures*60).to_i
+      @seconds =  (@seconds - @minutes*60 - @heures*3600).to_i
+
+      #Boucle d'affichage en fonction de nombre de secondes
+
+if @minutes >10
+      @minutes = "#{@minutes}"
+    else
+      @minutes = "0#{@minutes}"
+    end
+
+    if @seconds >10
+      @seconds = "#{@seconds}"
+    else
+      @seconds = "0#{@seconds}"
+    end
+
+   if @heures >10
+      @heures = "#{@heures}"
+    else
+      @heures = "0#{@heures}"
+    end
+
+
+    end
+
+    return "" + @heures +":" + @minutes +":" + @seconds
+  end
 end
