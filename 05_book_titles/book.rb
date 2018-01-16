412fe26de8168ea0435d3f1ab9158493a4114dc7
Mathieu Brun-Picard <b00673732@essec.edu>
2018-01-16 18:56:27 +0100

    Ex 5

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,9 +1,42 @@
-class Book
-attr_accessor :title
+#Book Titles
 
+class Book
+  def title
+    @title
+  end
+  def title=(string)
+    @title = capital_letters(string)
+    return @title
+  end
+
+  def capital_letters(title)
+    #declaration des variables
+    @title = title
+    @array_title = @title.split()
+    @capitalized_movies = []
+    @return = ""
+    i=0
+    @array_title.each do |x|
+
+
+      #Boucle if qui contient les exceptions
+
+      stop_words = %w(and in the of a an)
+      if stop_words.include?(x) && i!=0
+        @capitalized_movies <<  x
+      elsif
+        x = x.capitalize
+        @capitalized_movies <<  x
+      end
+      i=i+1
+    end
+#I take out the last " "
+@capitalized_movies.each do |x|
+  @return += x + " "
+end
+@return = @return.chomp(' ')
+return @return
 end
 
-@book = Book.new
-@book.title = "inferno"
-@cap_book_title = @book.title.capitalize
-puts @cap_book_title
+
+end
