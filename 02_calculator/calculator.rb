24b6357ae37680379f52cd2a0f831f9cb838ea9a
Mathieu Brun-Picard <b00673732@essec.edu>
2018-01-16 22:29:33 +0100

    With comments

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,3 +1,4 @@
+#fonction somme
 def add(a,b)
   @a=a
   @b=b
@@ -6,7 +7,7 @@ def add(a,b)
   return add
 
 end
-
+#fonction difference
 def subtract(a,b)
   @a=a
   @b=b
@@ -14,10 +15,13 @@ def subtract(a,b)
   return dif
 
 end
-
+#fonction difference
 def sum(array)
   @array = array
   @sum=0
+
+  #Si l'array est vide, je retourne 0
+  #sinon je fais la somme de tous les élements
   if @array.empty?
     return 0
 
@@ -29,6 +33,9 @@ def sum(array)
     return @sum
   end
 end
+
+#Avec * args en argument je peux prendre N entrées
+#Il ne me reste plus qu'a tout les multiplier entre elles
 
 def multiply(*args)
 
@@ -42,6 +49,7 @@ return product
 
 end
 
+#Definition de la fonction puissance
 def power(a,b)
   @a=a
   @b=b
@@ -49,7 +57,10 @@ def power(a,b)
   return @a**@b
 
 end
-
+#Pour la factorielle j'ai mis tout les valeurs de 1 à a
+#dans un array
+#Ensuite je parcours l'array pour multiplié tout les membres entreux eux
+#Vu que 0!=1 j'ai mis une condition pour tester si a > 1, sinon on a 1 en sortie
 def factorial(a)
   @a = a
   @facto = 1
